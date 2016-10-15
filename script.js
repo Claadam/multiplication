66f29fbabff2402dad0204d025bf87b847784193
WalnutsAreCool <claadam@nuevaschool.org>
2016-10-14 19:54:45 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("loaded")
+var a = $("#factor-one")
+var b = $("#factor-two")
+var product = $("#product")
+var input = $(".input")
+var results = $("#list")
+var list = [""]
+
+function multiplication (arg1,arg2) {
+  console.log("called")
+ var c = arg1*arg2
+ product.html(c)
+ list.push(" " + c)
+ results.html(list.toString())
+}
+
+input.keydown(function(e) {	
+  if (e.keyCode == "13") {
+    var aval = a.val()
+    var bval = b.val()
+    console.log("enter")
+    multiplication(aval,bval)
+  }
+})
